d71a6ef400316cf7e179ba9c310192ecbf30b632
Oleksiy <okorniychuk@proton.me>
2025-07-08 20:01:11 -0500

    Enhance API with version and world info endpoints. Added /get-versions to retrieve DFHack and Dwarf Fortress versions, and /get-world-info for game-specific details. Updated unit list endpoint and added TODOs for future features.

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi_mcp import FastApiMCP
 from dfhack_client_python.dfhack_remote import connect, close
-from dfhack_client_python.dfhack import GetVersion, GetUnitList
+from dfhack_client_python.dfhack import GetVersion, GetDFVersion, GetUnitList, GetWorldInfo, GetViewInfo
+from dfhack_client_python.py_export.BasicApi_pb2 import GetWorldInfoOut
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """Connect to DFHack on startup and close the connection when the server shuts down"""
     await connect()
+    #TODO: Need to prefetch and cache static data like materials, units, etc.
     yield
     await close()
 
@@ -19,15 +21,65 @@ app = FastAPI(
     lifespan=lifespan
 )
 
-@app.get("/get_dfhack_version", operation_id="get_dfhack_version")
-async def get_dfhack_version():
-    """Get the version of DFHack"""
-    return { "message": (await GetVersion()).value }
+@app.get("/get-versions", operation_id="get_versions")
+async def get_versions():
+    """Get the DFHack and Dwarf Fortress versions for connected server"""
+    dfhack_version = (await GetVersion()).value
+    dwarf_fortress_version = (await GetDFVersion()).value
+    return {
+        "dfhackVersion": dfhack_version,
+        "dwarfFortressVersion": dwarf_fortress_version
+    }
 
-@app.get("/get_unit_list", operation_id="get_unit_list")
+@app.get("/get-world-info", operation_id="get_world_info")
+async def get_world_info():
+    """Get the world specific info for the currently running game"""
+    world_info = await GetWorldInfo()
+    details = {}
+    if world_info.mode == GetWorldInfoOut.Mode.MODE_DWARF:
+        details = {
+            "civId": world_info.civ_id,
+            "siteId": world_info.site_id,
+            "groupId": world_info.group_id,
+            "raceId": world_info.race_id
+        }
+
+    return {
+        "worldName": {
+            "languageId": world_info.world_name.language_id,
+            "firstName": world_info.world_name.first_name,
+            "lastName": world_info.world_name.last_name,
+            "nickname": world_info.world_name.nickname,
+            "englishName": world_info.world_name.english_name
+        },
+        "mode": GetWorldInfoOut.Mode.Name(world_info.mode),
+        "details": details
+    }
+
+@app.get("/get-view-info", operation_id="get_view_info")
+async def get_view_info():
+    """Get the view info for the currently running game"""
+    view_info = await GetViewInfo()
+    return {
+        "viewInfo": view_info
+    }
+
+@app.get("/get-unit-list", operation_id="get_unit_list")
 async def get_unit_list():
-    """Get the list of units in the currently running Dwarf Fortress game"""
+    """Get the list of units in the currently running game"""
     return { "message": "Units: " + str(len((await GetUnitList()).creature_list)) }
+
+# TODO:
+# - RunCommand (Run a DFHack command)
+# - GetBlockList (Get MapBlock info for all blocks in a 3d volume)
+# - ListUnits (Get BasicUnitInfo on a specific list of units or a list of matching units based on filters)
+# - GetPlantList? (Not sure how usefull this is as the PlatDef type only gives a location and an index ...)
+# - GetUnitListInside (GetListOf)
+# - GetViewInfo (very useful for determining what the player is currently seeing)
+# - PassKeyboardEvent (for perfomring shortcuts for the player maybe. Might be better with RunCommmand)
+# - SetPauseState
+# - GetPauseState
+# - GetReports (Useful but returns a lot of info, might need a way to cache and filter the data)
 
 mcp = FastApiMCP(
     app,
